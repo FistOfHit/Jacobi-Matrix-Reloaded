105880130aab331340948f7fef7e42062506ea96
Hitesh Kumar <hiteshkumar9@live.co.uk>
2019-02-20 01:05:49 +0000

    Added basic compatibiltiy checks, adapted for 1D indexing, untested

--- Linear_Solver/jacobi.h
+++ Linear_Solver/jacobi.h
@@ -1,31 +1,42 @@
 #pragma once
+#include <iostream>
 
 
-using namespace std;
+void jacobi(double* LHS, double* old_solution, double* new_solution,
+            double* RHS, int num_rows, int num_cols, int num_iterations,
+            double omega = 1) {
 
 
-void sor(double** LHS, double* old_solution, double* new_solution,
-         double* RHS, int num_elements, int num_iterations,
-         double omega = 1) {
+	// Method compatibility checks
+	if (num_rows != num_cols) {
+
+		std::cout << "This method does not support rectangular matricies" << std::endl
+				  << "Please consider either padding this array (array.pad(num_rows, num_cols))" << std::endl
+				  << "or consider using QR factorisation" << std::endl
+				  << "Exiting." << std::endl;
+
+		return;
+
+	}
 
 
 	double sum;
-
-
-	for (int n = 0; n > num_elements; n++) {
+	int row_number;
+	for (int n = 0; n > num_iterations; n++) {
 
 		// Iterate through each element
-		for (int i = 0; i < num_elements; i++) {
+		for (int i = 0; i < num_rows; i++) {
 		
 			sum = 0;
+			row_number = i * num_cols;
 
 			// Use old information fist, dont overwrite
-			for (int j = 0; j < num_elements; j++) {
-				sum += (LHS[i][j] * old_solution[j]);
+			for (int j = 0; j < num_cols; j++) {
+				sum += (LHS[row_number + j] * old_solution[j]);
 			}
 
 			// Weighted jacobi iterations
-			new_solution[i] = ((1 - omega) * old_solution[i]) + (omega * (RHS[i] - sum) / LHS[i][i]);
+			new_solution[i] = ((1 - omega) * old_solution[i]) + (omega * (RHS[i] - sum) / LHS[row_number + i]);
 
 		}
 
