b355523e17c3bcb70890a6eeb7503b5325992562
Hitesh Kumar <hiteshkumar9@live.co.uk>
2019-02-20 14:42:35 +0000

    Added some tests that can be run optionally to check stability of matrix class

--- Linear_Solver/main.cpp
+++ Linear_Solver/main.cpp
@@ -1,15 +1,78 @@
 #include <iostream>
 #include <math.h>
 #include <time.h>
-#include "Matrix.h"
 #include "Matrix.cpp"
+
+
+using namespace std;
+
+
+void creation_tests() {
+
+
+	// Lets test if we can create a matrix and then
+	// allocate values to it that were allocated before.
+	cout << "----------Test: Create pre-allocated matrix----------" << endl;
+
+	// Manually allocating array
+	int prealloc_test_array[25];
+	for (int i = 0; i < 25; i++) { prealloc_test_array[i] = i+1; }
+
+	// Creating a matrix
+	Matrix<int>test_matrix(5, 5, prealloc_test_array);
+
+	cout << "Expected print out: " << endl << endl
+		 << "Matrix 5 X 5" << endl
+		 << "Data type: int" << endl
+		 << "1 2 3 4 5" << endl
+		 << "6 7 8 9 10" << endl
+		 << "11 12 13 14 15" << endl
+		 << "16 17 18 19 20" << endl
+		 << "21 22 23 24 25" << endl;
+
+	test_matrix.print();
+
+
+	// Lets test if we can create an empty matrix
+	// (full of zeros) and print it out
+	cout << "----------Test: Create empty zeros matrix----------" << endl;
+
+	// Creating a matrix
+	Matrix<int>test_matrix(5, 5, true);
+
+	cout << "Expected print out: " << endl << endl
+		 << "Matrix 5 X 5" << endl
+		 << "Data type: int" << endl
+		 << "0 0 0 0 0" << endl
+		 << "0 0 0 0 0" << endl
+		 << "0 0 0 0 0" << endl
+		 << "0 0 0 0 0" << endl
+		 << "0 0 0 0 0" << endl;
+
+	test_matrix.print();
+
+
+}
+
+void run_tests() {
+
+	creation_tests();
+
+}
+
+
 
 int index_convert(int row_ind, int col_ind, int row_count) {
 	return row_ind * row_count + col_ind;
 }
 
 int main() {
-	std::cout << "Hello World";
+
+
+	// If you want to run the tests
+	//run_tests();
+
+
 	int rows = 3;
 	int cols = 3;
 	auto *A = new Matrix<double>(rows, cols, true);
@@ -54,5 +117,7 @@ int main() {
 
 	delete A;
 	delete L;
+
+
 	system("pause");
 }
