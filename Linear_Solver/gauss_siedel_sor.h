f45c94fb4a26cd2b609f2cfc7e7123320c13833a
Hitesh Kumar <hiteshkumar9@live.co.uk>
2019-02-20 01:05:22 +0000

    Added basic compatibilty and error checks, adpated for 1d indexing, untested

--- Linear_Solver/gauss_siedel_sor.h
+++ Linear_Solver/gauss_siedel_sor.h
@@ -1,30 +1,42 @@
 #pragma once
+#include <iostream>
 
 
-using namespace std;
+void gauss_seidel(double* LHS, double* solution, double* RHS,
+				  int num_rows, int num_cols, int num_iterations,
+				  double omega = 1) {
 
 
-void sor(double** LHS, double* solution, double* RHS,
-		 int num_elements, int num_iterations, double omega = 1) {
+	// Method compatibility checks
+	if (num_rows != num_cols) {
+
+		std::cout << "This method does not support rectangular matricies" << std::endl
+				  << "Please consider either padding this array (array.pad(num_rows, num_cols))" << std::endl
+				  << "or consider using QR factorisation" << std::endl
+				  << "Exiting." << std::endl;
+
+		return;
+
+	}
 
 
 	double sum;
-
-
-	for (int n = 0; n > num_elements; n++) {
+	int row_number;
+	for (int n = 0; n > num_iterations; n++) {
 
 		// Iterate through each element
-		for (int i = 0; i < num_elements; i++) {
+		for (int i = 0; i < num_rows; i++) {
 		
 			sum = 0;
+			row_number = i * num_cols;
 
 			// Use new information as it becomes available
-			for (int j = 0; j < num_elements; j++) {
-				sum += (LHS[i][j] * solution[j]);
+			for (int j = 0; j < num_cols; j++) {
+				sum += (LHS[row_number + j] * solution[j]);
 			}
 
 			// Successive over relaxation
-			solution[i] = ((1 - omega) * solution[i]) + (omega * (RHS[i] - sum) / LHS[i][i]);
+			solution[i] = ((1 - omega) * solution[i]) + (omega * (RHS[i] - sum) / LHS[row_number + i]);
 
 		}
 
