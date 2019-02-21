aa6eee0ef43535443a04595c8aa88f67b8e6f224
Hitesh Kumar <hiteshkumar9@live.co.uk>
2019-02-21 17:24:23 +0000

    committing stupid code

--- Linear_Solver/Solvers.cpp
+++ Linear_Solver/Solvers.cpp
@@ -1,13 +1,17 @@
+#pragma once
 #include <iostream>
 #include "Solvers.h"
 #include "Matrix.h"
 #include "Matrix.cpp"
 #include <assert.h>
+#include <algorithm>
 
 
+// Transpose any matrix
 Matrix<double>* Solver::transpose(Matrix<double> *A) {
 	auto *A_t = new Matrix<double>(A->num_rows, A->num_cols, true);
-	A_t->fill_zeros(); // A_t is Upper triangular Empty
+
+	// Iterate through elements, swapping
 	for (int i = 0; i < A->num_rows; i++) {
 		for (int j = 0; j < A->num_cols; j++) {
 			A_t->values[j * A->num_rows + i] = A->values[i* A->num_rows + j];
@@ -16,157 +20,10 @@ Matrix<double>* Solver::transpose(Matrix<double> *A) {
 	return A_t;
 }
 
+// Checks if inputs are actually valid fror the defined
+// problem before solver proceeds
+void Solver::check_input_validity() {
 
-void Solver::form_LUD(Matrix<double> *U, Matrix<double> *L) {
-	assert(A->num_rows == A->num_cols);
-	for (int j = 0; j < A->num_cols; j++) {
-		for (int i = 0; i < j + 1; i++) {
-			double s1 = 0;
-
-			for (int k = 0; k < i; k++) {
-				s1 += U->values[k * A->num_rows + j] * L->values[i * A->num_rows + k];
-			}
-			U->values[i * A->num_rows + j] = A->values[i * A->num_rows + j] - s1;
-		}
-		for (int i = j; i < A->num_cols; i++) {
-			double s2 = 0;
-			for (int k = 0; k < j; k++) {
-				s2 += U->values[k * A->num_rows + j] * L->values[i * A->num_rows + k];
-			}
-			L->values[i * A->num_rows + j] = (A->values[i * A->num_rows + j] - s2) / U->values[j * A->num_rows + j];
-		}
-	}
-}
-
-
-void Solver::form_cholesky(Matrix<double> *L) {
-	assert(A->num_rows == A->num_cols);
-	for (int i = 0; i < A->num_rows; i++) {
-		for (int j = 0; j < (i + 1); j++) {
-			double s = 0;
-			for (int k = 0; k < j; k++) {
-				s += (L->values[i * A->num_rows + k] * L->values[j * A->num_rows + k]);
-			}
-			if (i == j) {
-				L->values[i * A->num_rows + j] = sqrt(A->values[i * A->num_rows + i] - s);
-			}
-			else {
-				L->values[i * A->num_rows + j] = (1.0 / L->values[j * A->num_rows + j] * (A->values[i * A->num_rows + j] - s));
-			}
-		}
-	}
-}
-
-
-Matrix<double>* Solver::forward_substitution(Matrix<double> *L, Matrix<double> *y) {
-	assert(L->num_rows == L->num_cols);
-	for (int i = 0; i < L->num_rows; i++) {
-		if (i == 0) {
-			y->values[i] = B->values[i] / L->values[i  * L->num_rows + i];
-		}
-		else if (i != 0) {
-			double s = 0;
-			for (int j = 0; j < i; j++) {
-				s += L->values[i * L->num_rows + j] * y->values[j];
-			}
-			y->values[i] = (1 / L->values[i * L->num_rows + i]) * (B->values[i] - s);
-		}
-	}
-	return y;
-}
-
-
-void Solver::backward_substitution(Matrix<double> *U, Matrix<double> *y) {
-	assert(U->num_rows == U->num_cols);
-	for (int i = U->num_rows; i>-1; i--) {
-		if (i == U->num_rows - 1) {
-			x->values[U->num_rows - 1] = y->values[U->num_rows - 1] / U->values[i *  U->num_rows + i];
-		}
-		else if (i != U->num_rows) {
-			double s = 0;
-			for (int j = 0; j < U->num_cols; j++) {
-				s += U->values[i* U->num_rows + j] * x->values[j];
-			}
-			x->values[i] = (1 / U->values[i * U->num_rows + i]) * (y->values[i] - s);
-		}
-	}
-}
-
-
-void Solver::LUD_solve() {
-	// U and L the same shape as A
-	auto *U = new Matrix<double>(A->num_rows, A->num_cols, true);
-	auto *L = new Matrix<double>(A->num_rows, A->num_cols, true);
-	// y and x the same shape as B
-	auto *y = new Matrix<double>(B->num_rows, B->num_cols, true);
-
-	U->fill_zeros();
-	y->fill_zeros();
-	x->fill_zeros();
-
-	// Decompose into upper and lower triagular matrices
-	// No scope issues as both declaration and function called within this function
-	form_LUD(U, L);
-	// Forward substitution
-	y = forward_substitution(L, y);
-	// Backward substitution
-	backward_substitution(U, y);
-	delete U;
-	delete L;
-	delete y;
-}
-
-
-void Solver::cholesky_solve() {
-	// Initialise empty matrices for lower and lower transpose
-	auto *L = new Matrix<double>(A->num_rows, A->num_cols, true);
-	auto *L_t = new Matrix<double>(A->num_rows, A->num_rows, true);
-	// y and x the same shape as B
-	auto *y = new Matrix<double>(B->num_rows, B->num_cols, true);
-
-	L_t->fill_zeros();
-	y->fill_zeros();
-	x->fill_zeros();
-
-	// Decompose A matrix into lower triangular
-	form_cholesky(L);
-	// Form upper triangular from lower triangular
-	L_t = transpose(L);
-	// Forward substitution
-	y = forward_substitution(L, y);
-	delete L;
-	// Backward substitution
-	backward_substitution(L_t, y);
-	delete L_t;
-}
-
-
-void Solver::gauss_seidel_solve(int num_iterations, double omega) {
-	double sum;
-	int row_index;
-	for (int n = 0; n < num_iterations; n++) {
-		// Iterate through each element
-		for (int i = 0; i < A->num_rows; i++) {
-
-			sum = 0;
-			row_index = i * A->num_cols;
-
-			// Use new information as it becomes available
-			for (int j = 0; j < i; j++) {
-				sum += (A->values[row_index + j] * x->values[j]);
-			}
-			for (int j = i + 1; j < A->num_cols; j++) {
-				sum += (A->values[row_index + j] * x->values[j]);
-			}
-			// Successive over relaxation
-			x->values[i] = ((1 - omega) * x->values[i]) +
-				(omega * (B->values[i] - sum) / A->values[row_index + i]);
-		}
-	}
-}
-
-
-void Solver::jacobi_solve(int num_iterations, double omega) {
 	// Method valildty check
 	if (A->num_rows != A->num_cols) {
 
@@ -193,15 +50,211 @@ void Solver::jacobi_solve(int num_iterations, double omega) {
 			<< "Exiting." << std::endl << std::endl;
 		return;
 	}
+
+}
+
+
+// Perform the decomposition to L and U
+void Solver::form_LUD(Matrix<double> *U, Matrix<double> *L) {
+	// For square matrices only
+	assert(A->num_rows == A->num_cols);
+	for (int j = 0; j < A->num_cols; j++) {
+		for (int i = 0; i < j + 1; i++) {
+			double s1 = 0;
+			// Subtract values of previous UL's
+			for (int k = 0; k < i; k++) {
+				s1 += U->values[k * A->num_rows + j] * L->values[i * A->num_rows + k];
+			}
+			U->values[i * A->num_rows + j] = A->values[i * A->num_rows + j] - s1;
+		}
+		for (int i = j; i < A->num_cols; i++) {
+			double s2 = 0;
+			// Add values of previous UL's
+			for (int k = 0; k < j; k++) {
+				s2 += U->values[k * A->num_rows + j] * L->values[i * A->num_rows + k];
+			}
+			L->values[i * A->num_rows + j] = (A->values[i * A->num_rows + j] - s2) / U->values[j * A->num_rows + j];
+		}
+	}
+}
+
+
+// Perform decomposition to L and L*
+void Solver::form_cholesky(Matrix<double> *L) {
+	// For square matrices only
+	assert(A->num_rows == A->num_cols);
+	for (int i = 0; i < A->num_rows; i++) {
+		for (int j = 0; j < (i + 1); j++) {
+			double s = 0;
+			// Add or subtract values of previous UL's
+			for (int k = 0; k < j; k++) {
+				s += (L->values[i * A->num_rows + k] * L->values[j * A->num_rows + k]);
+			}
+			if (i == j) {
+				L->values[i * A->num_rows + j] = sqrt(A->values[i * A->num_rows + i] - s);
+			}
+			else {
+				L->values[i * A->num_rows + j] = (1.0 / L->values[j * A->num_rows + j] * (A->values[i * A->num_rows + j] - s));
+			}
+		}
+	}
+}
+
+
+// Perform the forward substitution to solve Lx = b
+Matrix<double>* Solver::forward_substitution(Matrix<double> *L, Matrix<double> *y) {
+	// For square matrices only
+	assert(L->num_rows == L->num_cols);
+	for (int i = 0; i < L->num_rows; i++) {
+		if (i == 0) {
+			y->values[i] = B->values[i] / L->values[i  * L->num_rows + i];
+		}
+		else if (i != 0) {
+			double s = 0;
+			for (int j = 0; j < i; j++) {
+				s += L->values[i * L->num_rows + j] * y->values[j];
+			}
+			y->values[i] = (1 / L->values[i * L->num_rows + i]) * (B->values[i] - s);
+		}
+	}
+	return y;
+}
+
+
+// Perform the backwards substitution to solve L*z = y
+void Solver::backward_substitution(Matrix<double> *U, Matrix<double> *y) {
+	assert(U->num_rows == U->num_cols);
+	for (int i = U->num_rows; i>-1; i--) {
+		if (i == U->num_rows - 1) {
+			x->values[U->num_rows - 1] = y->values[U->num_rows - 1] / U->values[i *  U->num_rows + i];
+		}
+		else if (i != U->num_rows) {
+			double s = 0;
+			for (int j = 0; j < U->num_cols; j++) {
+				s += U->values[i* U->num_rows + j] * x->values[j];
+			}
+			x->values[i] = (1 / U->values[i * U->num_rows + i]) * (y->values[i] - s);
+		}
+	}
+}
+
+
+void Solver::LUD_solve() {
+
+	// Checking to see if problem is valid
+	check_input_validity();
+
+	// U and L the same shape as A
+	auto *U = new Matrix<double>(A->num_rows, A->num_cols, true);
+	auto *L = new Matrix<double>(A->num_rows, A->num_cols, true);
+	// y and x the same shape as B
+	auto *y = new Matrix<double>(B->num_rows, B->num_cols, true);
+
+	U->fill_zeros();
+	y->fill_zeros();
+	x->fill_zeros();
+
+	// Decompose into upper and lower triagular matrices
+	// No scope issues as both declaration and function called within this function
+	form_LUD(U, L);
+	// Forward substitution
+	y = forward_substitution(L, y);
+	// Backward substitution
+	backward_substitution(U, y);
+	delete U;
+	delete L;
+	delete y;
+}
+
+
+void Solver::cholesky_solve() {
+
+	// Checking to see if problem is valid
+	check_input_validity();
+
+	// Initialise empty matrices for lower and lower transpose
+	auto *L = new Matrix<double>(A->num_rows, A->num_cols, true);
+	auto *L_t = new Matrix<double>(A->num_rows, A->num_rows, true);
+	// y and x the same shape as B
+	auto *y = new Matrix<double>(B->num_rows, B->num_cols, true);
+
+	L_t->fill_zeros();
+	y->fill_zeros();
+	x->fill_zeros();
+
+	// Decompose A matrix into lower triangular
+	form_cholesky(L);
+	// Form upper triangular from lower triangular
+	L_t = transpose(L);
+	// Forward substitution
+	y = forward_substitution(L, y);
+	delete L;
+	// Backward substitution
+	backward_substitution(L_t, y);
+	delete L_t;
+}
+
+
+void Solver::gauss_seidel_solve(double omega) {
+
+	// Checking to see if problem is valid
+	check_input_validity();
+
+	double sum;
+	double max_change = 1;
+	double new_value;
+	int row_index;
+
+	double tolerance = 0.0001;
+	while (max_change > tolerance) {
+		max_change = 0;
+		// Iterate through each element
+		for (int i = 0; i < A->num_rows; i++) {
+
+			sum = 0;
+			row_index = i * A->num_cols;
+
+			// Use new information as it becomes available
+			for (int j = 0; j < i; j++) {
+				sum += (A->values[row_index + j] * x->values[j]);
+			}
+			for (int j = i + 1; j < A->num_cols; j++) {
+				sum += (A->values[row_index + j] * x->values[j]);
+			}
+			// Successive over relaxation
+			new_value = ((1 - omega) * x->values[i]) +
+				(omega * (B->values[i] - sum) / A->values[row_index + i]);
+
+			// Check to see if tolerance has been reached
+			max_change = std::max(abs(x->values[i] - new_value), max_change);
+
+			x->values[i] = new_value;
+
+		}
+	}
+}
+
+
+void Solver::jacobi_solve(double omega) {
+
+	// Checking to see if problem is valid
+	check_input_validity();
+	
 	// Create array to store old solution for jacobi
 	double *old_solution = new double[x->num_rows];
 	double sum;
+	double max_change = 1;
 	int row_index;
 
-	for (int n = 0; n < num_iterations; n++) {
-		for (int k = 0; k < x->num_rows; k++) {
-			old_solution[k] = x->values[k];
-		}
+	for (int k = 0; k < x->num_rows; k++) {
+		old_solution[k] = x->values[k];
+	}
+
+	double tolerance = 0.0001;
+	while (max_change > tolerance) {
+
+		max_change = 0;
+		
 		// Iterate through each element
 		for (int i = 0; i < A->num_rows; i++) {
 			sum = 0;
@@ -216,6 +269,17 @@ void Solver::jacobi_solve(int num_iterations, double omega) {
 			// Weighted jacobi iteration
 			x->values[i] = ((1 - omega) *  x->values[i]) +
 				(omega * (B->values[i] - sum) / A->values[row_index + i]);
+		}
+
+
+		// Check to see if tolerance has been reached
+		for (int k = 0; k < x->num_rows; k++) {
+			max_change = std::max(abs(old_solution[k] - x->values[k]), max_change);
+		}
+
+		// Replace old with new and iterate onwards
+		for (int k = 0; k < x->num_rows; k++) {
+			old_solution[k] = x->values[k];
 		}
 	}
 	delete[] old_solution;
--- Linear_Solver/Solvers.h
+++ Linear_Solver/Solvers.h
@@ -1,25 +1,32 @@
-#ifndef SOLVERS
-#define SOLVERS
-
+#pragma once
 #include "Matrix.h"
 
 
+// Class that loads the problem matrices,
+// and implements the solvers.
 class Solver{	
 
 	public:
 
+		// Problem defintiion
 		Matrix<double> *A;			// A matrix
 		Matrix<double> *B;			// B matrix
 		Matrix<double> *x;			// X solution matrix
 
+		// To check if the inputs are valid.
+		void check_input_validity();
+		// Transposing any matrix
 		Matrix<double>* transpose(Matrix<double> *A);
+		// Create the L and U/L* for the direct methods
 		void form_LUD(Matrix<double> *U, Matrix<double> *L);
 		void form_cholesky(Matrix<double> *L);
+		// Forward/backwards substitution sub-routines
 		Matrix<double>* forward_substitution(Matrix<double> *L, Matrix<double> *y);
 		void backward_substitution(Matrix<double> *U, Matrix<double> *y);
+		// Solvers themselves
 		void LUD_solve();
 		void cholesky_solve();
-		void gauss_seidel_solve(int num_iterations = 100, double omega = 1);
-		void jacobi_solve(int num_iterations = 100, double omega = (2.0 / 3.0));
+		void gauss_seidel_solve(double omega = 1.0);
+		void jacobi_solve(double omega = (2.0 / 3.0));
+
 };
-#endif
