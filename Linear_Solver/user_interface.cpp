594790a3efa64573a4baccdb57c5929d78f56dae
Hitesh Kumar <hiteshkumar9@live.co.uk>
2019-02-21 19:12:47 +0000

    beginning of user interface

---
+++ Linear_Solver/user_interface.cpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+
+using namespace std;
+
+
+void print_options() {
+
+	cout << "-------- The Jacobian Matrix Reloaded --------" << endl << endl
+		 << "Lets start by loading up the problem (A and B)." << endl
+		 << "You have the following choices: " << endl
+		 << "1) Run our pre-defined tests for all of our solvers"
+		 << "2) Randomly create a ";
+
+
+}
