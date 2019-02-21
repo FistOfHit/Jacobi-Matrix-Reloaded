841c9998f85aad2153c255376d70ec000662e999
Hitesh Kumar <hiteshkumar9@live.co.uk>
2019-02-21 00:15:28 +0000

    FIXED!!! removed summation of j=i instance

--- Linear_Solver/gauss_siedel_sor.h
+++ Linear_Solver/gauss_siedel_sor.h
@@ -3,28 +3,34 @@
 #include "Matrix.cpp"
 
 
-void gauss_seidel(Matrix<double> *LHS, Matrix<double> *solution,
-				  Matrix<double> *RHS, int num_iterations, double omega) {
+using namespace std;
+
+
+void gauss_seidel(Matrix<double> &LHS, Matrix<double> &solution,
+				  Matrix<double> &RHS, int num_iterations, double omega = 1) {
 
 
 	double sum;
 	int row_index;
-	for (int n = 0; n > num_iterations; n++) {
+	for (int n = 0; n < num_iterations; n++) {
 
 		// Iterate through each element
-		for (int i = 0; i < LHS->num_rows; i++) {
+		for (int i = 0; i < LHS.num_rows; i++) {
 		
 			sum = 0;
-			row_index = i * LHS->num_cols;
+			row_index = i * LHS.num_cols;
 
 			// Use new information as it becomes available
-			for (int j = 0; j < LHS->num_cols; j++) {
-				sum += (LHS->values[row_index + j] * solution->values[j]);
+			for (int j = 0; j < i; j++) {
+				sum += (LHS.values[row_index + j] * solution.values[j]);
+			}
+			for (int j = i+1; j < LHS.num_cols; j++) {
+				sum += (LHS.values[row_index + j] * solution.values[j]);
 			}
 
 			// Successive over relaxation
-			solution->values[i] = ((1 - omega) * solution->values[i]) +
-								  (omega * (RHS->values[i] - sum) / LHS->values[row_index + i]);
+			solution.values[i] = ((1 - omega) * solution.values[i]) +
+								  (omega * (RHS.values[i] - sum) / LHS.values[row_index + i]);
 
 		}
 
